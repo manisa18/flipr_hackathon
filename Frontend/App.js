8eca1a7e764499ef056af22266d560e0e74c0434
Aakash Gupta <aakashgupta228@gmail.com>
2023-04-22 01:57:39 +0530

    1st commit

---
+++ Frontend/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Navbar, Feed, VideoDetail, AudioDetail, SearchFeed} from './components';
+
+
+const App = () => {
+    <BrowserRouter>
+        <Box sx={{backgroundColor: '#000'}}>
+            <Navbar />
+            <Routes>
+                <Route path='/' exact element={<Feed />} />
+                <Route path='/video/:id' element={<VideoDetail />} />
+                <Route path='/audio/:id' element={<AudioDetail />} />
+                <Route path='/search/:searchTerm' element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
+};
+
+export default App
